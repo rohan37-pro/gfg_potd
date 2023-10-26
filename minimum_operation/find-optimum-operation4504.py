cd15abbd603b0ade4d3b3714d951655f63c39f0d
rohan37-pro <rohan2002maji@gmail.com>
2023-10-26 07:39:03 -0700

    user inputs are taken, input function is added

--- minimum_operation/find-optimum-operation4504.py
+++ minimum_operation/find-optimum-operation4504.py
@@ -10,3 +10,9 @@ class Solution:
                 op += 1
         return op
 
+if __name__ == "__main__":
+    t = int(input("enter number of tabs : "))
+    for i in range(t):
+        n = int(input("enter : "))
+        ob = Solution()
+        print(ob.minOperation(n))
