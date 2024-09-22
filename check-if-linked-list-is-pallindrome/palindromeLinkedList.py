7375d20589782410939b01591730c6a975298380
rohan37-pro <rohan2002maji@gmail.com>
2024-09-22 05:42:30 +0530

    don't need extra comments

--- check-if-linked-list-is-pallindrome/palindromeLinkedList.py
+++ check-if-linked-list-is-pallindrome/palindromeLinkedList.py
@@ -1,4 +1,4 @@
-#User function Template for python3
+
 '''
 	Your task is to check if given linkedlist
 	is a pallindrome or not.
@@ -7,16 +7,13 @@
 	Return Type: boolean , no need to print just return True or False.
 
 	{
-		# Node Class
 		class Node:
 		    def __init__(self, data):   # data -> value stored in node
 		        self.data = data
 		        self.next = None
 	}
-
-	Contributed By: Nagendra Jha
 '''
-#Function to check whether the list is palindrome.
+
 class Solution:
     def isPalindrome(self, head):
         pass
