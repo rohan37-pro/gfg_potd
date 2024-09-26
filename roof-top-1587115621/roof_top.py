e039fdc5843a03b4ae3e75265460d2a4074e8790
rohan37-pro <rohan2002maji@gmail.com>
2024-09-26 15:53:50 +0530

    roof-top problem is solved with python time taken to run is 0.42sec.

--- roof-top-1587115621/roof_top.py
+++ roof-top-1587115621/roof_top.py
@@ -1,8 +1,17 @@
 #User function Template for python3
-
 class Solution:
     
     #Function to find maximum number of consecutive steps 
     #to gain an increase in altitude with each step.
     def maxStep(self, arr):
-        #Your code here
+        maximum = 0
+        Current = 0
+        elem = arr[0]
+        for i in arr[1:]:
+            if i>elem:
+                Current+=1
+                maximum = max(maximum, Current)
+            else:
+                Current = 0
+            elem = i
+        return maximum
