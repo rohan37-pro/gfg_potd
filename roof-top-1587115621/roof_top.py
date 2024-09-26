8cd651af459bcedc85dcd8630422e6442d9edeba
rohan37-pro <rohan2002maji@gmail.com>
2024-09-26 16:08:47 +0530

    remove the comments from the solution of roof-top

--- roof-top-1587115621/roof_top.py
+++ roof-top-1587115621/roof_top.py
@@ -1,8 +1,5 @@
-#User function Template for python3
+
 class Solution:
-    
-    #Function to find maximum number of consecutive steps 
-    #to gain an increase in altitude with each step.
     def maxStep(self, arr):
         maximum = 0
         Current = 0
