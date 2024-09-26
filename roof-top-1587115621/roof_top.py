8704cd98ad6cffcdfa5a8398a469ca41d7aa61c8
rohan37-pro <rohan2002maji@gmail.com>
2024-09-26 14:51:57 +0530

    roof-top.py snippit created

---
+++ roof-top-1587115621/roof_top.py
@@ -0,0 +1,8 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find maximum number of consecutive steps 
+    #to gain an increase in altitude with each step.
+    def maxStep(self, arr):
+        #Your code here
