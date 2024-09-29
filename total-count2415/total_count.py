e357274fc7e9de0c41eba9c1ce0d76ceb0142650
rohan37-pro <rohan2002maji@gmail.com>
2024-09-29 11:47:54 +0530

    total count problem solved with python

--- total-count2415/total_count.py
+++ total-count2415/total_count.py
@@ -2,5 +2,10 @@
 
 class Solution:
     def totalCount(self, k, arr):
-        # code here
+        sum = 0
+        for i in arr:
+            sum += (i//k)
+            if i%k!=0:
+                sum+=1
+        return sum
         
