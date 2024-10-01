6dffe2e3e776ebc789dac253d37d1bbd4efbf1c8
rohan37-pro <rohan2002maji@gmail.com>
2024-10-01 10:05:40 +0530

    boiler code uploaded

---
+++ multiply-two-linked-lists/multiply_two_linked_lists.py
@@ -0,0 +1,15 @@
+# your task is to complete this function
+# Function should return an integer value
+# head1 denotes head node of 1st list
+# head2 denotes head node of 2nd list
+
+'''
+class node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+'''
+
+class Solution:
+    def multiply_two_lists(self, first, second):
+        # Code here
