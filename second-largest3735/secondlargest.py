e220d4437f3f234e8299297aa3e9f65bd3e07cba
rohan37-pro <rohan2002maji@gmail.com>
2024-11-15 13:04:44 +0530

    second solution found which is solved long ago but not very efficient

--- second-largest3735/secondlargest.py
+++ second-largest3735/secondlargest.py
@@ -1,3 +1,4 @@
+# optimum solution takes 0.31sec..
 class Solution:
     def getSecondLargest(self, arr):
         second = -1
@@ -9,3 +10,15 @@ class Solution:
             elif i>second and i!=large:
                 second = i
         return second
+
+#second solution runs in 0.47sec
+class Solution:
+    def getSecondLargest(self, arr):
+        arr.sort()
+        larg = arr[-1]
+        larg2 = -1
+        for i in arr[-1::-1]:
+            if i < larg:
+                larg2 = i
+                break
+        return larg2
