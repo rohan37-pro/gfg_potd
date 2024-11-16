150845fef9164b1a6b0d37e3c7f60353225be259
rohan37-pro <rohan2002maji@gmail.com>
2024-11-16 12:07:22 +0530

    this new solution exceeds the time limit

--- move-all-zeroes-to-end-of-array0751/moveAllZeroes.py
+++ move-all-zeroes-to-end-of-array0751/moveAllZeroes.py
@@ -1,3 +1,5 @@
+
+# this solution takes 0.55sec to run all the samples of gfg
 class Solution:
 	def pushZerosToEnd(self,arr):
 		temp = []
@@ -6,6 +8,25 @@ class Solution:
 			if i==0:
 				zero +=1
 			else:
-			    temp.append(i)
+				temp.append(i)
 		t = len(temp)
-		for i in temp
+		for i in range(t):
+			arr[i] = temp[i]
+		for i in range(t, t+zero):
+			arr[i] = 0
+
+
+
+# this solution exceeds the time limit runs in O(n^2)
+class Solution:
+	def pushZerosToEnd(self,arr):
+		zero = 0
+		i = 0
+		while i < len(arr):
+			if arr[i]==0:
+				arr.pop(i)
+				zero +=1
+			else:
+				i+=1
+		for i in range(zero):
+			arr.append(0)
