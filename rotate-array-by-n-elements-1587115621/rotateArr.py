2a70caf1c57ab507cd7d091458d1200353ed7118
rohan37-pro <rohan2002maji@gmail.com>
2024-11-18 19:33:49 +0530

    array is successfully rotated to the left by n in 0.6sec with time complexity O(2d+n)

--- rotate-array-by-n-elements-1587115621/rotateArr.py
+++ rotate-array-by-n-elements-1587115621/rotateArr.py
@@ -1,6 +1,24 @@
+# this easiest solution is taking too much time. time complexity O(d*n) longer then 2.07
 class Solution:
     def rotateArr(self, arr, d):
+        d = d%len(arr) 
         for i in range(d):
             a = arr.pop(0)
             arr.append(a)
-    
+
+
+# this solution takes 0.56sec to run time complexity O(2d+n)
+class Solution:
+    def rotateArr(self, arr, d):
+        n = len(arr)
+        d = d%n
+        temp = []
+        for i in range(d):
+            temp.append(arr[i])
+        for i in range(n-d):
+            arr[i] = arr[i+d]
+        j = n-d
+        for i in temp:
+            arr[j] = i
+            j+=1
+        
